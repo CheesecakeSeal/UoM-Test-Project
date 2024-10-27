b89715ebbacda474515a1f5e5fe2a8f4ad84c6c7
CheesecakeSeal <cheesecakeseal@gmail.com>
2024-10-27 10:43:13 +0100

    Added Validation for Operator

--- src/net/cheesecakeseal/testcalc/TestCalc.java
+++ src/net/cheesecakeseal/testcalc/TestCalc.java
@@ -10,22 +10,32 @@ public class TestCalc{
         double num1 = 0;
         double num2 = 0;
         double result = 0;
-        char operator = 'a';
+        char operator = '0';
+        boolean success = false;
 
         System.out.println("--Calculator---");
 
-        // Ask User for first number
+        // Ask User for first number, Error may appear here if user enter anything other than a number
         System.out.print("Enter first number: ");
         num1 = sc.nextDouble();
-        // Ask User for the operation
-        System.out.print("Enter an operator (+, -, *, /): ");
-        operator = sc.next().charAt(0);
-        //Ask User for first number
+
+        // Ask User for the operation, make sure it is valid (This may be ideal to automate testing)
+        do {
+            System.out.print("Enter an operator (+, -, *, /): ");
+            operator = sc.next().charAt(0);
+            if(operator == '+'| operator == '-'| operator == '*'| operator == '/'){
+                success = true;
+            }else{
+                System.out.println("Invalid Operator | Valid Operators Include +, -, *, / | Retry");
+            }
+        }while(!success);
+
+        //Ask User for first number, Error may appear here if user enter anything other than a number
         System.out.print("Enter second number: ");
         num2 = sc.nextDouble();
 
 
-        // Perform calculation based on operator
+        // Switch case based on operator to perform calculation as specified
         switch (operator) {
             case '+':
                 result = num1 + num2;
